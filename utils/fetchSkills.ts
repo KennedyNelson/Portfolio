cd35306ecd6e95aa0d4b74d0aaa1a0ef8262d6b6
Kennedy Nelson <Kennedy.Nelson@ibm.com>
2022-11-22 15:23:24 +0530

    bug fix

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -2,7 +2,7 @@ import { Skill } from '../typings'
 
 export const fetchSkills = async() => {
     const dev = process.env.NODE_ENV !== 'production';
-    const res = await fetch(`${dev ? 'http://localhost:3000' : 'https://myportfolio-ecru-nu.vercel.app/'}/api/Skills`);
+    const res = await fetch(`${dev ? 'http://localhost:3000' : 'https://myportfolio-ecru-nu.vercel.app/'}/api/getSkills`);
 
 
     const data = await res.json()
