5cdd846e1e83aebab98d1275937d536b29a4f490
Kennedy Nelson <Kennedy.Nelson@ibm.com>
2022-11-23 15:10:15 +0530

    UI

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,7 +33,7 @@ export default function Hero({pageInfo}: Props) {
             <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>{pageInfo.role}</h2>
             <h1 className='text-3xl sm:text-5xl lg:text-6xl font-semibold px-10'>
                 <span>{text}</span>
-                <Cursor cursorColor='#ccc' />
+                <Cursor cursorColor='#F7AB0A' />
             </h1>
             <div className='pt-5'>
                 <Link href='#about'><button className='heroButton'>About</button></Link>
